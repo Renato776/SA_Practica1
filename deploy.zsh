2c9293a066d719c7398a640ca2e4d8c47cd65495
Renato Flores <renatojosuefloresperez@gmail.com>
2021-06-03 22:58:14 -0600

    Updated readme

--- deploy.zsh
+++ deploy.zsh
@@ -1,7 +1,7 @@
 #!/bin/env zsh
 
 TARGET=~/bin
-cd cp -r $TARGET/dist ~/dist
+cp -r $TARGET/dist/* ~/dist
 cd ~/dist
-s3 sync --delete s3://usac-frontend .
+aws s3 sync --delete s3://usac-frontend .
 echo Application successfully deployed.
